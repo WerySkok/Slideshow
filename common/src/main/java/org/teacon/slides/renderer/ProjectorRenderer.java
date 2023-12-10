32c4cbae02ac19f6eb6651e4c11b779a2f338291
LX862 <kenny.mh.hui@outlook.com>
2023-12-10 22:48:55 +0800

    Don't request slide if not powered

--- common/src/main/java/org/teacon/slides/renderer/ProjectorRenderer.java
+++ common/src/main/java/org/teacon/slides/renderer/ProjectorRenderer.java
@@ -37,58 +37,61 @@ public class ProjectorRenderer extends BlockEntityRendererMapper<ProjectorBlockE
 //        renderBoundingBox(pStack, tile);
 
 		// always update slide state
-		final Slide slide = SlideState.getSlide(tile.mLocation, !tile.mDisableLod);
 		final boolean projectorDisabled = tile.getBlockState().getValue(BlockStateProperties.POWERED);
+		if(projectorDisabled) return;
+
+		final Slide slide = SlideState.getSlide(tile.mLocation, !tile.mDisableLod);
+
 		if (slide == null) {
 			return;
 		}
 
-		if (!projectorDisabled) {
-			int color = tile.mColor;
-			if ((color & 0xFF000000) == 0) {
-				return;
-			}
+		// Render
+        int color = tile.mColor;
+        if ((color & 0xFF000000) == 0) {
+            return;
+        }
 
-			pStack.pushPose();
+        pStack.pushPose();
 
-			PoseStack.Pose last = pStack.last();
-			Matrix4f pose = last.pose();
-			Matrix3f normal = last.normal();
+        PoseStack.Pose last = pStack.last();
+        Matrix4f pose = last.pose();
+        Matrix3f normal = last.normal();
 
-			BlockState state = tile.getBlockState();
-			// get direction
-			Direction direction = state.getValue(BlockStateProperties.FACING);
-			// get internal rotation
-			ProjectorBlock.InternalRotation rotation = state.getValue(ProjectorBlock.ROTATION);
-			// matrix 1: translation to block center
-			pStack.translate(0.5f, 0.5f, 0.5f);
-			// matrix 2: rotation
-			pose.multiply(direction.getRotation());
-			normal.mul(direction.getRotation());
-			// matrix 3: translation to block surface
-			pStack.translate(0.0f, 0.5f, 0.0f);
-			// matrix 4: internal rotation
-			rotation.transform(pose);
-			rotation.transform(normal);
-			// matrix 5: translation for slide
-			pStack.translate(-0.5F, 0.0F, 0.5F - tile.mHeight);
-			// matrix 6: offset for slide
-			pStack.translate(tile.mOffsetX, -tile.mOffsetZ, tile.mOffsetY);
-			// matrix 7: scaling
-			pose.multiply(Matrix4f.createScaleMatrix(tile.mWidth, 1.0F, tile.mHeight));
+        BlockState state = tile.getBlockState();
+        // get direction
+        Direction direction = state.getValue(BlockStateProperties.FACING);
+        // get internal rotation
+        ProjectorBlock.InternalRotation rotation = state.getValue(ProjectorBlock.ROTATION);
+        // matrix 1: translation to block center
+        pStack.translate(0.5f, 0.5f, 0.5f);
+        // matrix 2: rotation
+        pose.multiply(direction.getRotation());
+        normal.mul(direction.getRotation());
+        // matrix 3: translation to block surface
+        pStack.translate(0.0f, 0.5f, 0.0f);
+        // matrix 4: internal rotation
+        rotation.transform(pose);
+        rotation.transform(normal);
+        // matrix 5: translation for slide
+        pStack.translate(-0.5F, 0.0F, 0.5F - tile.mHeight);
+        // matrix 6: offset for slide
+        pStack.translate(tile.mOffsetX, -tile.mOffsetZ, tile.mOffsetY);
+        // matrix 7: scaling
+        pose.multiply(Matrix4f.createScaleMatrix(tile.mWidth, 1.0F, tile.mHeight));
 
-			final boolean flipped = tile.getBlockState().getValue(ProjectorBlock.ROTATION).isFlipped();
+        final boolean flipped = tile.getBlockState().getValue(ProjectorBlock.ROTATION).isFlipped();
 
-			slide.render(source, last.pose(), last.normal(), tile.mWidth, tile.mHeight, color, packedLight,
-					OverlayTexture.NO_OVERLAY, flipped || tile.mDoubleSided, !flipped || tile.mDoubleSided, !tile.mDisableLod,
-					SlideState.getAnimationTick(), partialTick);
+        slide.render(source, last.pose(), last.normal(), tile.mWidth, tile.mHeight, color, packedLight,
+                OverlayTexture.NO_OVERLAY, flipped || tile.mDoubleSided, !flipped || tile.mDoubleSided, !tile.mDisableLod,
+                SlideState.getAnimationTick(), partialTick);
 
-			pStack.popPose();
-		}
+        pStack.popPose();
 
-		boolean canShowDiscordHighlight = Minecraft.getInstance().player.isCreative() || Minecraft.getInstance().player.isSpectator();
+        boolean canShowDiscordHighlight = Minecraft.getInstance().player.isCreative() || Minecraft.getInstance().player.isSpectator();
 
-		if(Config.getDiscordVisualizerEnabled() && !projectorDisabled && canShowDiscordHighlight) {
+		// Render Discord Highlight
+		if(Config.getDiscordVisualizerEnabled() && canShowDiscordHighlight) {
 			try {
 				URI uri = new URI(tile.mLocation);
 				if(uri.getHost().contains("discord")) {
